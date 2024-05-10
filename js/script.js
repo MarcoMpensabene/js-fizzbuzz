f7fcf4ae0095bd41aa899382ccd04f848a310bd3
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-10 17:07:58 +0200

    added cicle for 1-100 and the   logical condition

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+for(let i = 1 ; i <= 100 ; i++) {
+    if(i % 3 === 0){
+        i = "Fizz";
+    } else if (i % 5 === 0){
+        i = "Buzz";
+    }else if ((i % 3 === 0) && (i % 5 === 0)){
+        i = "FizzBuzz";
+    }
+    console.log(i)
+}
+
