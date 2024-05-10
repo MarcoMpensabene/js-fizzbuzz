42d5b9c3aad987e0837f73dfefe4017136ecf71c
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-10 17:28:09 +0200

    added 3 var and changed the condition statment position

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-
+let fizz = "fizz";
+let buzz = "buzz";
+let fizzBuzz = "FizzBuzz";
 for(let i = 1 ; i <= 100 ; i++) {
-    if(i % 3 === 0){
-        i = "Fizz";
-    } else if (i % 5 === 0){
-        i = "Buzz";
-    }else if ((i % 3 === 0) && (i % 5 === 0)){
-        i = "FizzBuzz";
+    if ((i % 3 === 0) && (i % 5 === 0)){
+        console.log(fizzBuzz)
+    } else if (i % 3 === 0){
+        console.log(fizz)
+    }else if  ((i % 5 === 0)){
+        console.log(buzz)
+    }  else{
+        console.log(i)
     }
-    console.log(i)
+
+
 }
 
