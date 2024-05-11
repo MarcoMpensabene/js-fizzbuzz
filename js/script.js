6ebcbd5b97c25dc28b9628f7dbc0fc1fb4a679fd
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-11 15:58:32 +0200

    added all the elements of the cicle with append() on the container and used css for position them

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
-let fizz = "fizz";
-let buzz = "buzz";
+let fizz = "Fizz";
+let buzz = "Buzz";
 let fizzBuzz = "FizzBuzz";
 for(let i = 1 ; i <= 100 ; i++) {
     if ((i % 3 === 0) && (i % 5 === 0)){
         console.log(fizzBuzz)
+        const div = document.querySelector("div.container");
+        const p = document.createElement("p");
+        p.append(fizzBuzz);
+        div.append(p);
     } else if (i % 3 === 0){
         console.log(fizz)
+        const div = document.querySelector("div.container");
+        const p = document.createElement("p");
+        p.append(fizz);
+        div.append(p);
     }else if  ((i % 5 === 0)){
         console.log(buzz)
+        const div = document.querySelector("div.container");
+        const p = document.createElement("p");
+        p.append(buzz);
+        div.append(p);
     }  else{
         console.log(i)
+        const div = document.querySelector("div.container");
+        const p = document.createElement("p");
+        p.append(i);
+        div.append(p);
     }
 
 
 }
-
