6f044d0993cd35310f0c2766a814c7ba0534d794
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-11 16:10:24 +0200

    added class for every node element that i added on the container so i can modify it with css

--- js/script.js
+++ js/script.js
@@ -5,27 +5,31 @@ for(let i = 1 ; i <= 100 ; i++) {
     if ((i % 3 === 0) && (i % 5 === 0)){
         console.log(fizzBuzz)
         const div = document.querySelector("div.container");
-        const p = document.createElement("p");
-        p.append(fizzBuzz);
-        div.append(p);
+        const fizzbuzzP = document.createElement("p");
+        fizzbuzzP.className = "fizzbuzz";
+        fizzbuzzP.append(fizzBuzz);
+        div.append(fizzbuzzP);
     } else if (i % 3 === 0){
         console.log(fizz)
         const div = document.querySelector("div.container");
-        const p = document.createElement("p");
-        p.append(fizz);
-        div.append(p);
+        const fizzP = document.createElement("p");
+        fizzP.className = "fizz";
+        fizzP.append(fizz);
+        div.append(fizzP);
     }else if  ((i % 5 === 0)){
         console.log(buzz)
         const div = document.querySelector("div.container");
-        const p = document.createElement("p");
-        p.append(buzz);
-        div.append(p);
+        const buzzP = document.createElement("p");
+        buzzP.className = "buzz";
+        buzzP.append(buzz);
+        div.append(buzzP);
     }  else{
         console.log(i)
         const div = document.querySelector("div.container");
-        const p = document.createElement("p");
-        p.append(i);
-        div.append(p);
+        const number = document.createElement("p");
+        number.className = "number";
+        number.append(i);
+        div.append(number);
     }
 
 
